624f3d0feadcdbf7e93dc9a507693586b97006c8
SoleSM <miliserrrano@gmail.com>
2022-09-09 16:42:02 -0300

    actualizacion

--- src/views/informacion/Info.jsx
+++ src/views/informacion/Info.jsx
@@ -3,38 +3,44 @@ import imagen2 from '../../assets/imagen2.jpg'
 import imagen3 from '../../assets/imagen3.jpg'
 import CardImagen from '../../components/Cards/CardImagen'
 import styled from 'styled-components'
+import { SSRProvider } from 'react-bootstrap'
 
 const Container = styled.div`
-margin : 5px 5px 5px 5px;
-background-color: #C7FFDE ; 
+margin: 10px 10px 10px 10px;
+background-color: #9ADFFE;
+
 `;
 const Title = styled.h1`
   font-size: bold;
   text-align: center;
-  color: rgba(88.00000235438347, 116.00000068545341, 231.00000143051147, 1);
+  color: black;
   font-family: concert one;
   padding: 20px
 `;
 const Title2 = styled.h3`
 color: black;
-font-family: CenturyGothic;
-padding: 20px
+padding: 20px;
+font-size: 30px;
+font-family: Montserrat;
+font-weight: bold;
 `;
 const Parrafo = styled.p`
   flex-direction: row;
   justify-content: left;
   text-align: justify;
-  font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif;
+  font-family: Montserrat;
   text-indent: 10px;
-  margin: 40px;
+  padding: 70px;
+  font-size: 20px;
+  
 
 `;
 const Wrapper = styled.div`
-height: 300px;
+height: 50%;
 display: flex;
 `;
 const Left = styled.div`
-padding: 20px
+padding: 40px
 width: 50%;
 height: 50vh
 `;
@@ -44,16 +50,26 @@ height: 50vh;
 padding: 20px
 `;
 
-const ButtonQuestions = styled.button`
-display: inline-block;
-color: palevioletred;
-font-size: 1em;
-margin: 1em;
-padding: 0.25em 1em;
-border: 2px solid palevioletred;
-border-radius: 3px;
-display: block;
+const Preguntas = styled.div`
+width: 30%;
+height: 100vh;
+padding: 20px;
+background-color: #2CC9A8
 `;
+
+const Button = styled.button`
+  display: inline-block;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  display: block;
+`;
+
+
+
 
 
 export default function Info() {
@@ -61,11 +77,11 @@ export default function Info() {
   return (
     <>
 
-      <Container>
+      <Container >
         <Title>SOBERANIA ALIMENTARIA FORMOSEÑA</Title>
-        <Wrapper>
+        <Wrapper >
           <Left>
-           <Title2>¿QUÉ ES?</Title2>
+            <Title2>¿Qué es?</Title2>
             <Parrafo>
               En Formosa, el Gobierno provincial lanzó un nuevo programa que beneficia a la economía de las familias y a los pequeños
               productores conocidos como paipperos(forman parte del PAIPPA).
@@ -77,7 +93,7 @@ export default function Info() {
           </Right>
         </Wrapper>
 
-        <Wrapper>
+        <Wrapper >
           <Right>
             <CardImagen
               imagen={imagen2} />
@@ -92,7 +108,7 @@ export default function Info() {
           </Left>
         </Wrapper>
 
-        <Wrapper>
+        <Wrapper data-aos="fade-up">
           <Left>
             <Parrafo>
               Con esto se buscar impulsar a la producción y consumo local, mejorar la nutrición, la seguridad alimentaria y ayudar al bolsillo
@@ -107,7 +123,32 @@ export default function Info() {
           </Right>
         </Wrapper>
 
-        <ButtonQuestions>PREGUNTAS FRECUENTES</ButtonQuestions>
+ 
+        <Wrapper data-aos="fade-up">
+          <Right>
+            <CardImagen
+              imagen={imagen2} />
+          </Right>
+          <Left>
+            <Parrafo>
+              <strong>Soberanía Alimentaria Formosa</strong> es un programa que ofrece alimentos directamente del productor al consumidor, es decir,
+              sin que existan intermediarios y agregados de valor entre la comercialización y las familias. Así, la gente puede acceder
+              a una gran variedad de alimentos de producción local a precios referenciados. Desde frutas, verduras, hortalizas, carnes,
+              productos lácteos y artesanales de los distintos consorcios adheridos al programa.
+            </Parrafo>
+          </Left>
+        </Wrapper>
+
+        <Wrapper data-aos="fade-up-right">
+        <Preguntas>
+          <Title2>PREGUNTAS FRECUENTES</Title2>
+          <Button>Soy productor, cómo puedo estar en la feria?</Button>
+          <Button>Soy productor, cómo puedo estar en la feria?</Button>
+          <Button>Soy productor, cómo puedo estar en la feria?</Button>
+          <Button>Soy productor, cómo puedo estar en la feria?</Button>
+        </Preguntas>
+        </Wrapper>
+
 
       </Container>
 
